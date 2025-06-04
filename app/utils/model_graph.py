c89c5f1463c492a3e09890eb47d797799c8d49f0
Usman Aftab <usman.aftab@gmail.com>
2025-06-05 00:47:54 +0100

    added logging

--- app/utils/model_graph.py
+++ app/utils/model_graph.py
@@ -8,6 +8,10 @@ from langgraph.graph.message import add_messages
 from langchain_tavily import TavilySearch
 from langgraph.prebuilt import ToolNode, tools_condition
 
+from app.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -20,16 +24,22 @@ def chatbot(state: State):
     return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 
+logger.info("Initializing TavilySearch tool")
+
 tool = TavilySearch(max_results=2)
 tools = [tool]
 
+logger.info("Initializing chat model")
 llm = init_chat_model(
     "eu.amazon.nova-micro-v1:0",
     model_provider="bedrock",
+    region_name="eu-west-1",
 )
 
+logger.info("Binding tools to the chat model")
 llm_with_tools = llm.bind_tools(tools)
 
+logger.info("Creating chatbot node")
 graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
 
@@ -43,3 +53,4 @@ graph_builder.add_edge("tools", "chatbot")
 
 graph_builder.add_edge(START, "chatbot")
 graph = graph_builder.compile()
+logger.info("Chatbot graph created successfully")
