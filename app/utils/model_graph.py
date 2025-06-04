9146db1c038b68f08396dd8bbd3f9f9150f6d63c
Usman Aftab <usman.aftab@gmail.com>
2025-06-04 17:24:59 +0100

    added model tools

--- app/utils/model_graph.py
+++ app/utils/model_graph.py
@@ -5,6 +5,8 @@ from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START
 from langgraph.graph.message import add_messages
+from langchain_tavily import TavilySearch
+from langgraph.prebuilt import ToolNode, tools_condition
 
 
 class State(TypedDict):
@@ -15,16 +17,29 @@ class State(TypedDict):
 
 
 def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
+
+tool = TavilySearch(max_results=2)
+tools = [tool]
 
 llm = init_chat_model(
     "eu.amazon.nova-micro-v1:0",
     model_provider="bedrock",
 )
 
+llm_with_tools = llm.bind_tools(tools)
 
 graph_builder = StateGraph(State)
 graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+
 graph_builder.add_edge(START, "chatbot")
 graph = graph_builder.compile()
